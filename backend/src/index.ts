bb3409445bf340d1c3277e17685f3d3564f9d844
kavinda-KPD <kavindadissanayake1999@gmail.com>
2024-01-29 11:12:50 +0530

    V13 Implement The Product Page

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,6 +14,11 @@ app.get('/api/products', (req: Request, res: Response) => {
     res.json(sampleProducts)
 })
 
+app.get('/api/products/:slug', (req: Request, res: Response) => {
+    console.log(req.body);
+    res.json(sampleProducts.find((x) => x.slug === req.params.slug))
+  })
+
 const PORT = 4000
 
 app.listen(PORT, () => {
